751939f9e268faba4f7130b82f7127d016f5896e
Luka <kazicluka4@gmail.com>
2026-07-25 14:00:43 +0200

    Caching the products and flushing

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,13 +6,18 @@ use App\Http\Requests\StoreProductRequest;
 use App\Models\Products;
 use App\Repositories\ProductRepository;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class ProductController extends Controller
 {
 
     public function index(ProductRepository $productRepository)
     {
-        $products = $productRepository->getLatestProducts(10);
+        $products = Products::hydrate(
+            Cache::remember('latestProducts', 500, function () use($productRepository) {
+                return $productRepository->getLatestProducts(10)->toArray();
+            })
+        );
         return view('products.index', compact('products'));
     }
 
@@ -24,6 +29,11 @@ class ProductController extends Controller
     public function store(StoreProductRequest $request) {
         Products::query()->create($request->validated());
         return redirect()->route('products.index')->with('success', 'Product created successfully!');
+    }
+
+    public function flush() {
+        Cache::forget('latestProducts');
+        return redirect()->route('products.index');
     }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ Route::get('/', [ProductController::class, 'index'])->name('products.index');
 
 
 Route::controller(ProductController::class)->prefix('/products')->name('products.')->group(function () {
+    Route::get('/flush', 'flush')->name('flush');
     Route::get('/create', 'create')->name('create');
     Route::post('/store', 'store')->name('store');
 });
