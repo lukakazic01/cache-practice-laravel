923a183917e201b0a0baf4ab255f8177b0d98c41
lukakazic01 <kazicluka4@gmail.com>
2026-07-28 10:38:49 +0200

    Login and register view + fortify config for them

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -29,6 +29,8 @@ class FortifyServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Fortify::registerView('auth.register');
+        Fortify::loginView('auth.login');
         Fortify::createUsersUsing(CreateNewUser::class);
         Fortify::updateUserProfileInformationUsing(UpdateUserProfileInformation::class);
         Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
