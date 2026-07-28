ab77b6582f80ec62fe24e51a338fac616bb511e8
lukakazic01 <kazicluka4@gmail.com>
2026-07-28 15:05:00 +0200

    Used image intervention integration with Laravel, transformed image into webp

--- app/Services/ProfileService.php
+++ app/Services/ProfileService.php
@@ -3,8 +3,10 @@
 namespace App\Services;
 
 use App\Http\Requests\NewAvatarRequest;
-use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
+use Intervention\Image\Exceptions\InvalidArgumentException;
+use Intervention\Image\FileExtension;
+use Intervention\Image\Interfaces\ImageManagerInterface;
 
 class ProfileService
 {
@@ -14,13 +16,20 @@ class ProfileService
         //
     }
 
-    public function changeAvatar(NewAvatarRequest $request) {
-        $avatar = auth()->user()->avatar;
-        if ($avatar && Storage::disk('public')->exists('images/avatars/'.$avatar)) {
-            Storage::disk('public')->delete('images/avatars/'.$avatar);
-        }
-        $path = $request->file('profile_image')->store('images/avatars', 'public');
-        $name = basename($path);
+    /**
+     * @throws InvalidArgumentException
+     */
+    public function changeAvatar(NewAvatarRequest $request, ImageManagerInterface $imageManager) {
+//        $avatar = auth()->user()->avatar;
+//        if ($avatar && Storage::disk('public')->exists('images/avatars/'.$avatar)) {
+//            Storage::disk('public')->delete('images/avatars/'.$avatar);
+//        }
+//        $path = $request->file('profile_image')->store('images/avatars', 'public');
+//        $name = basename($path);
+        $name = uniqid() . '.webp';
+        $file = $request->file('profile_image');
+        $path = Storage::disk('public')->path('images/avatars/' . $name);
+        $imageManager->decode($file)->encodeUsingFileExtension(FileExtension::WEBP, quality: 90)->save($path);
         auth()->user()->update(['avatar' => $name]);
     }
 }
