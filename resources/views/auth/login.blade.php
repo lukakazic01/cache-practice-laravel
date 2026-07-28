5a077c4a87101d994665c621729d0dd36848cb42
lukakazic01 <kazicluka4@gmail.com>
2026-07-28 11:09:18 +0200

    Put layout inside login view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,14 +1,16 @@
-<form method="POST" action="{{ route('login.store') }}" class="flex flex-col gap-6">
-    @csrf
-    <x-forms.field name="email" required>
-        <x-forms.label>Email</x-forms.label>
-        <x-forms.input type="email" :value="old('email', '')" />
-        <x-forms.error-message />
-    </x-forms.field>
-    <x-forms.field name="password" required>
-        <x-forms.label>Password</x-forms.label>
-        <x-forms.input :value="old('password', '')" />
-        <x-forms.error-message />
-    </x-forms.field>
-    <x-base.button type="submit">Submit</x-base.button>
-</form>
+<x-layout>
+    <form method="POST" action="{{ route('login.store') }}" class="flex flex-col gap-6">
+        @csrf
+        <x-forms.field name="email" required>
+            <x-forms.label>Email</x-forms.label>
+            <x-forms.input type="email" :value="old('email', '')" />
+            <x-forms.error-message />
+        </x-forms.field>
+        <x-forms.field name="password" required>
+            <x-forms.label>Password</x-forms.label>
+            <x-forms.input :value="old('password', '')" />
+            <x-forms.error-message />
+        </x-forms.field>
+        <x-base.button type="submit">Submit</x-base.button>
+    </form>
+</x-layout>
