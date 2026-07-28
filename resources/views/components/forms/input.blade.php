fd45494eef4d68b7a5689d248717540991dd2cca
lukakazic01 <kazicluka4@gmail.com>
2026-07-28 10:45:44 +0200

    Created form input component

---
+++ resources/views/components/forms/input.blade.php
@@ -0,0 +1,20 @@
+@aware(['name','required'])
+@props(['value'])
+
+<input
+    name="{{ $name }}"
+    id="{{ $name }}"
+    value="{{ old($name, $value ?? '') }}"
+    @if ($required) required @endif
+    {{
+        $attributes
+            ->class([
+                'w-full rounded-lg border border-gray-300 px-3.5 py-2.5 text-sm text-gray-900 placeholder-gray-400
+                focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500',
+                'border-red-400 focus:ring-red-400 focus:border-red-400' => $errors->has($name),
+            ])
+            ->merge([
+                'type' => 'text',
+            ])
+    }}
+/>
